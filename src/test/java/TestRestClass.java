073702719bf40fd83f84c23538d270a4edfc74d6
lenakoleno <sofiya.yefremova@list.ru>
2022-02-09 22:23:58 +0300

    Initial commit

---
+++ src/test/java/TestRestClass.java
@@ -0,0 +1,48 @@
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.junit.Test;
+
+
+public class TestRestClass {
+
+    @Test
+    public void whenSendPostRequest_thenCorrect()
+        throws IOException {
+        String json = "{"
+            + "    \"folderId\": \"b1g21kjlspgecjh6if8h\",\n"
+            + "    \"texts\": [\"Automated testing\"],\n"
+            + "    \"targetLanguageCode\": \"ru\"\n"
+            + "}";
+
+        RequestBody body = RequestBody.create(
+            MediaType.parse("application/json"),
+            json
+        );
+
+        Request request = new Request.Builder()
+            .url("https://translate.api.cloud.yandex.net/translate/v2/translate")
+            .post(body)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Authorization", "Bearer t1.9euelZqLyJbNz46Wl5WOj5qPy8uUlu3rnpWakciXisjMlZWPzsvMmMySjZjl9Pd_f29v-e9kI3Dp3fT3Py5tb_nvZCNw6Q.7DRsxhzMQdShWyCDngx1tG0TxXUWi3Zzu3hV9Sspp7kU2tmtxjBtVxP25Q5Bj8w-OxFyELMb4CQxJTv6wWI7CQ")
+            .build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+
+        Object obj = new JsonParser().parse(response.body().string());
+        JsonObject jo = (JsonObject) obj;
+        String text = jo.get("translations").getAsJsonArray().get(0).getAsJsonObject().get("text").getAsString();
+        System.out.println(text);
+    }
+}
