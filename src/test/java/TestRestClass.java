6c5066f1fd2c8d6565354cebf23005fdd7a50fae
Смирнов Михаил Юрьевич (4038340) <musmirnov@dev.vtb>
2022-02-11 20:26:11 +0500

    settings for Allure

--- src/test/java/TestRestClass.java
+++ src/test/java/TestRestClass.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
-import org.junit.Test;
+import org.testng.annotations.Test;
 import utils.BuildJsonUtil;
 import utils.CommonUtils;
 
