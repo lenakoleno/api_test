11bf51003489d55464929437fca433b0ec826ba3
lenakoleno <sofiya.yefremova@list.ru>
2022-02-11 14:09:13 +0300

    token change

--- src/test/java/TestRestClass.java
+++ src/test/java/TestRestClass.java
@@ -12,15 +12,14 @@ import utils.CommonUtils;
 public class TestRestClass {
 
     static final String key =
-        "Bearer t1.9euelZrOjpHKiY7JkZDHi5OOlYyKlO3rnpWakciXisjMlZWPzsvMmMySjZjl8_cWf2tv-e8LAjlp_N3z91YtaW_57wsCOWn8.6CZSfxmfnmgVu9VP5mO0OoPqNiZ_MM6exyG06OY1Vao4U8EgJL6H1ANXMsmYt1DKH-B3PojzGlCwFF4vpbvcAw";
+        "Bearer t1.9euelZqTlJrPl5qPmomUjJucmcyej-3rnpWakciXisjMlZWPzsvMmMySjZjl9PdyeGZv-e8LTxL63fT3Midkb_nvC08S-g.SFQCM4VIu8kOtAexS5sEWYrWhTOwJGOCYJAUfUahoFZ0nos4wTwm7Dd185p9Q6IydjdIDKlQUdyv4cH1a2cZDQ";
 
     static final String url = "https://translate.api.cloud.yandex.net/translate/v2/translate";
 
     static final OkHttpClient client = new OkHttpClient();
 
     @Test
-    public void whenSendPostRequest_thenCorrectTest_id01()
-        throws IOException {
+    public void whenSendPostRequest_thenCorrectTest_id01() throws IOException {
         Call call = client.newCall(
             CommonUtils.createRequest((BuildJsonUtil.buildJson("b1g21kjlspgecjh6if8h", "Automated testing", "ru")), url,
                 key));
