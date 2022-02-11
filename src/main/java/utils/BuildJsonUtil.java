c136ae7453ba7903852ae77d84fa97e04c50b2e4
Смирнов Михаил Юрьевич (4038340) <musmirnov@dev.vtb>
2022-02-11 20:27:41 +0500

    refactoring code

--- src/main/java/utils/BuildJsonUtil.java
+++ src/main/java/utils/BuildJsonUtil.java
@@ -3,12 +3,13 @@ package utils;
 public final class BuildJsonUtil {
     private BuildJsonUtil() {
     }
+
     public static String buildJson(String folderId, String texts, String targetLanguageCode) {
         String json = "{"
-            + "    \"folderId\": \"" + folderId + "\",\n"
-            + "    \"texts\": [\"" + texts + "\"],\n"
-            + "    \"targetLanguageCode\": \"" + targetLanguageCode + "\"\n"
-            + "}";
+                + "    \"folderId\": \"" + folderId + "\",\n"
+                + "    \"texts\": [\"" + texts + "\"],\n"
+                + "    \"targetLanguageCode\": \"" + targetLanguageCode + "\"\n"
+                + "}";
         return json;
     }
 }
